ee749f2bc02f3218bb796fe443c9f2f53a8e06fc
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-06 07:01:44 -0800

    UC1 Added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
 ﻿using System;
 
-namespace LineComparision
+namespace LineCompUC1
 {
-    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Line Comparsion Problems");
+
+            int x1, x2, y1, y2;
+
+            Console.WriteLine("Please Enter X1:");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter X2:");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Y1:");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter Y2:");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double LineLength1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"Length of Line 1 is : {LineLength1}");
+
+        }
+    }
 }
