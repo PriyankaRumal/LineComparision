baa694f0731386efd0461a07ceca134ec43eab04
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-06 07:06:55 -0800

    UC2 added

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace LineCompUC1
+namespace LineCompUC2
 {
     internal class Program
     {
@@ -21,6 +21,29 @@ namespace LineCompUC1
 
             double LineLength1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
             Console.WriteLine($"Length of Line 1 is : {LineLength1}");
+
+            int l1, l2, m1, m2;
+
+            Console.WriteLine("Please Enter l1:");
+            l1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter l2:");
+            l2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter m1:");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter m2:");
+            m2 = Convert.ToInt32(Console.ReadLine());
+
+            double LineLength2 = Math.Sqrt(Math.Pow(l2 - l1, 2) + Math.Pow(m2 - m1, 2));
+            Console.WriteLine($"Length of Line 2 is : {LineLength2}");
+
+            if (int.Equals(LineLength1, LineLength2))
+            {
+                Console.WriteLine("Length Of Two Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Length Of Two Lines are Not Equal");
+            }
 
         }
     }
